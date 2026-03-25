076402a3de9c6319b486d0ca22549cefb165ffbb
knico05 <nicola@kuederli.com>
2026-03-25 16:19:32 +0100

    chore: remove temporary debug logging from main.js

--- main.js
+++ main.js
@@ -69,7 +69,6 @@ function createWindow() {
   // This eliminates the white/blank flash that would otherwise appear.
   win.once('ready-to-show', () => {
     win.show();
-  });
 
   // Wire up IPC handlers that need the window reference
   setupIPC(win);
